74fb2f71677a8c1652f100f3c27cc13f1bdf8261
AldrianLoberiano <loberianorian@gmail.com>
2026-03-17 21:37:33 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,22 @@ use Illuminate\Support\Facades\Route;
 Route::redirect('/', '/index.html');
 
 Route::get('/admin', function () {
-    return response()->file(public_path('admin.html'));
+    return response()->file(public_path('admin-home.html'));
+});
+
+Route::get('/admin/login', function () {
+    return response()->file(public_path('admin-login.html'));
+});
+
+Route::get('/admin/dashboard', function () {
+    return response()->file(public_path('admin-dashboard.html'));
 });
 
 Route::get('/verify', function () {
+    return response()->file(public_path('verify-home.html'));
+});
+
+Route::get('/verify/check', function () {
     return response()->file(public_path('verify.html'));
 });
 
