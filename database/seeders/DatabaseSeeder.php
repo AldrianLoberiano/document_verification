494b1fed03ca969b409fe305ec6066a9980e62cb
AldrianLoberiano <loberianorian@gmail.com>
2026-03-17 21:22:52 +0800

    Create DatabaseSeeder.php

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::query()->updateOrCreate(
+            ['email' => 'admin@docverify.local'],
+            [
+                'name' => 'System Admin',
+                'password' => Hash::make('Admin@12345'),
+                'role' => 'admin',
+            ]
+        );
+    }
+}
